c5a5b161ba9acb6b68d8dfc020ed37d96018c333
Oleg Smyrnov <smirnovoleg95@gmail.com>
2021-02-14 11:20:31 +0100

    diplom v1

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,13 +3,13 @@
 const userForm = new UserForm();
 
 userForm.loginFormCallback = data => {
-    ApiConnector.login(data, response => {
+    ApiConnector.login(data, (response) => {
         return response.success ? location.reload() : userForm.setLoginErrorMessage(response.error);
     });
 }
 
 userForm.registerFormCallback = data => {
-    ApiConnector.register(data, response => {
+    ApiConnector.register(data, (response) => {
         return response.success ? location.reload() : userForm.setRegisterErrorMessage(response.error);
     });
 }
